6fc57062593aa5f620ae9f104c4e8869fd910bfa
Manon Jacquin <manon.jacquin@gmail.com>
2019-03-25 21:48:46 +0000

    Creates CRUD methods for the traveller model

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,11 +29,21 @@ dest10 = Destination.create(:city => "Denali", :country => "Alaska, US")
 dest11 = Destination.create(:city => "Florianopolis", :country => "Brazil")
 dest12 = Destination.create(:city => "Amsterdam", :country => "The Netherlands")
 
-hiking = Activity.create(:activity_name => "hiking", :comment => "awesome trip", :rating => 4)
-photography = Activity.create(:activity_name => "photography", :comment => "great light, great people", :rating => 4)
-horse_riding = Activity.create(:activity_name => "horse_riding", :comment => "amazing nature", :rating => 4)
-surfing = Activity.create(:activity_name => "surfing", :comment => "good waves", :rating => 4)
+hiking = Activity.create(:activity_name => "hiking", :comment => "awesome trip", :rating => 4, :traveller_id => traveller4.id, :destination_id => dest2.id)
+photography = Activity.create(:activity_name => "photography", :comment => "great light, great people", :rating => 4, :traveller_id => traveller5.id, :destination_id => dest8.id)
+horse_riding = Activity.create(:activity_name => "horse_riding", :comment => "amazing nature", :rating => 3, :traveller_id => traveller10.id, :destination_id => dest9.id)
+surfing = Activity.create(:activity_name => "surfing", :comment => "good waves", :rating => 4, :traveller_id => traveller11.id, :destination_id => dest1.id)
+apple_picking = Activity.create(:activity_name => "apple picking", :comment => "blablabla", :rating => 3, :traveller_id => traveller9id, :destination_id => dest4.id)
+shopping = Activity.create(:activity_name => "shopping", :comment => "I will come back for sure", :rating => 2, :traveller_id => traveller1.id, :destination_id => dest5.id)
+wine_tour = Activity.create(:activity_name => "wine tour", :comment => "what a food and wine scene!!", :rating => 3, :traveller_id => traveller3.id, :destination_id => dest6.id)
+snow_shoewing = Activity.create(:activity_name => "snow shoewing", :comment => "Mindblowing nature", :rating => 5, :traveller_id => traveller2.id, :destination_id => dest7.id)
+night_photography = Activity.create(:activity_name => "night_photography", :comment => "Incredible people", :rating => 5, :traveller_id => traveller6.id, :destination_id => dest3.id)
+fishing = Activity.create(:activity_name => "fishing", :comment => "I've seen a bear!!", :rating => 1, :traveller_id => traveller8.id, :destination_id => dest12.id)
+kayak = Activity.create(:activity_name => " kayak", :comment => "Gimme more of kayak there", :rating => 4, :traveller_id => traveller9.id, :destination_id => dest11.id)
+skiing = Activity.create(:activity_name => "skiing", :comment => "Skiing with the best instructors", :rating => 3, :traveller_id => traveller7.id, :destination_id => dest10.id)
 
 
-traveller1.activities << hiking
-dest1.activities << hiking
+
+#
+# traveller1.activities << hiking
+# dest1.activities << hiking
--- lib/models/destination.rb
+++ lib/models/destination.rb
@@ -2,4 +2,7 @@ class Destination < ActiveRecord::Base
   has_many :activities
   has_many :travellers, through: :activities
 
+  validates :city, presence: true
+
+
 end
